c88a7bbef77eb66051d9613094cf95e5522725b7
AnTi <zipcod@yandex.ru>
2025-03-05 15:22:15 +0300

    change confing

--- app/config.py
+++ app/config.py
@@ -1,13 +1,21 @@
 #здесь мы будем забирать переменные из .env и работать с остальными переменными окружения программы Это делается с помощью pydentic settings 
 # https://docs.pydantic.dev/latest/concepts/pydantic_settings/
 
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
-# Загружаем переменные из .env файла
-load_dotenv()
 
-# Получаем значения из переменных окружения
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
+class Settings(BaseSettings):
+    POSTGRES_PASSWORD: str
+    POSTGRES_USER: str
+    POSTGRES_DB: str
+#TODO дописать переменную модел кофиг так так чтобы путь к env был в явном виде
+
+# POSTGRES_PASSWORD=123
+# POSTGRES_USER=oleg
+# POSTGRES_DB=comments
+
+settings = Settings()
+
+
+if __name__ == "__main__":
+    print(settings)
